9512081dd11c4183724c836c715fa2107812da28
sebastianocampo-lab <sebastian.ocampo@uao.edu.co>
2026-09-23 11:06:13 -0500

    Delete challenge-04 directory

--- challenge-04/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { PaginaLibros } from './paginas/pagina-libros/pagina-libros';
-
-export const routes: Routes = [
-  { path: '', component: PaginaLibros, title: 'Challenge 04 - Pila de libros' },
-  { path: '**', redirectTo: '' },
-];
